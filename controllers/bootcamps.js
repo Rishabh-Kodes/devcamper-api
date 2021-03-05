2cf5d69b743e25ca1029b7c8397b1e2a9bc8a153
rishabh-kodes <rishabhkodes@gmail.com>
2021-03-05 23:41:17 +0530

    Advance Filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,18 @@ const Bootcamp = require("../models/Bootcamp");
 // @route           GET /api/v1/bootcamps
 // @access          Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
@@ -85,13 +96,10 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   // Earth Radius = 3,963 mi / 6,378 km
   const radius = distance / 3963;
 
-  // const bootcamps = await Bootcamp.find({
-  //   location: { $geoWithin: { $centerSphere: [ [ lat, lng ], radius ] }},
-  // });
-
   const bootcamps = await Bootcamp.find({
     location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
   });
+
   res
     .status(200)
     .json({ success: true, count: bootcamps.length, data: bootcamps });
